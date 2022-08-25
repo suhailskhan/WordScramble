6cda87a78905f7aa19557731bd4db95e55c49c75
Suhail Khan <26044674+suhailskhan@users.noreply.github.com>
2022-08-24 21:23:01 -0700

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Suhail Khan on 8/24/22.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
